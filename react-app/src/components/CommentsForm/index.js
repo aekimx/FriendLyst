31ebd97268d5b2dae652dcca31b36a607582a7d6
Aileen Kim <aileenkim96@gmail.com>
2023-03-24 00:37:35 -0400

    not sure what this issue is

--- react-app/src/components/CommentsForm/index.js
+++ react-app/src/components/CommentsForm/index.js
@@ -1,9 +1,32 @@
+import React, { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { createCommentThunk } from "../../store/comment";
 
 
-export default function CommentsForm() {
+export default function CommentsForm({postId}) {
+  const dispatch = useDispatch()
+
+
+  const [comment, setComment] = useState("")
+
+  const user = useSelector(state => state.session.user)
+  let userId;
+  if (user) userId = user.id
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newComment = {comment, userId, postId}
+    dispatch(createCommentThunk(newComment))
+    setComment('')
+  }
 
   return (
-    null
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input placeholder='comment' value={comment}
+        onChange={(e) => setComment(e.target.value)}/>
+      </form>
+      </div>
   )
 
 }
