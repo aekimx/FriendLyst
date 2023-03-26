6ee7e77870e562395a2bcc4f4c510dec5c9cc481
Aileen Kim <aileenkim96@gmail.com>
2023-03-26 00:39:15 -0400

    fixed typo

--- react-app/src/store/user.js
+++ react-app/src/store/user.js
@@ -2,6 +2,7 @@
 const GET_USER_PROFILE = 'user/GET_USER_PROFILE'
 const CREATE_USER_PROFILE = 'user/CREATE_USER_PROFILE'
 const UPDATE_USER_PROFILE = 'user/UPDATE_USER_PROFILE'
+const GET_USER_POSTS = 'posts/GET_USER_POSTS'
 
 // ----------------------------------- action creators   ---------------------------------
 const getUserProfile = (user) => ({
@@ -14,6 +15,10 @@ const updateUserProfile = (user) => ({
   user
 })
 
+const getUserPosts = (posts) => ({
+  type: GET_USER_POSTS,
+  posts
+})
 
 // ----------------------------------- thunks  ----------------------------------------
 
@@ -58,6 +63,16 @@ export const updateUserThunk = (userId, userProfile) => async (dispatch) => {
   }
 }
 
+export const getUserPostsThunk = (userId) => async (dispatch) => {
+  const res = await fetch(`/api/posts/${userId}/posts`)
+
+  if (res.ok) {
+    let data = await res.json()
+    dispatch(getUserPosts(data))
+    return data
+  }
+}
+
 
 const initialState = { user: null };
 
@@ -65,16 +80,23 @@ const initialState = { user: null };
 export default function userReducer(state = initialState, action) {
   let newState = {}
 	switch (action.type) {
-		case GET_USER_PROFILE:
+
+    case GET_USER_PROFILE:
       newState = { ...action.user}
 			return newState
+
     case UPDATE_USER_PROFILE:
       newState = { ...action.user}
       return newState
+
     case CREATE_USER_PROFILE:
       newState = {...action.newUser}
       return newState
-		default:
+
+    case GET_USER_POSTS:
+      newState = {...state}
+
+      default:
 			return state;
 	}
 }
